1f012b324ea8b101d152ee40c0508f32289196d1
Mart van Buren <mart@fractic.io>
2025-03-18 20:14:18 +0000

    [presentation] Include accounts and assertions when printing ledger.

--- src/impl/presentation/hledger_printer.rs
+++ src/impl/presentation/hledger_printer.rs
@@ -10,6 +10,16 @@ impl Account {
             Account::Expense(s) => format!("Expenses:{}", s.0),
         }
     }
+
+    fn type_tag(&self) -> char {
+        match self {
+            Account::Asset(_) => 'A',
+            Account::Cash(_) => 'C',
+            Account::Liability(_) => 'L',
+            Account::Income(_) => 'R',
+            Account::Expense(_) => 'X',
+        }
+    }
 }
 
 pub(crate) struct HledgerPrinter;
@@ -21,6 +31,18 @@ impl HledgerPrinter {
 
     pub(crate) fn print_ledger(&self, financial_records: &FinancialRecords) -> String {
         let mut ledger_output = String::new();
+
+        ledger_output.push_str("; ------------ Accounts ------------\n");
+        for account in &financial_records.accounts {
+            ledger_output.push_str(&format!(
+                "account {}  ; type: {}\n",
+                account.ledger(),
+                account.type_tag()
+            ));
+        }
+        ledger_output.push_str("\n\n\n");
+
+        ledger_output.push_str("; ------------ Transactions ------------\n");
         for tx in &financial_records.transactions {
             let label = financial_records.label_lookup.get(&tx.spec_id).map_or(
                 "(unknown)".to_string(),
@@ -50,6 +72,21 @@ impl HledgerPrinter {
             }
             ledger_output.push('\n');
         }
+        ledger_output.push_str("\n\n\n");
+
+        ledger_output.push_str("; ------------ Assertions ------------\n");
+        for assertion in &financial_records.assertions {
+            ledger_output.push_str(&format!("{} <assertion>\n", assertion.date));
+            ledger_output.push_str(&format!(
+                "    {:30} 0 == {:10.2} {}\n",
+                assertion.account.ledger(),
+                assertion.balance,
+                assertion.currency,
+            ));
+            ledger_output.push('\n');
+        }
+        ledger_output.push_str("\n\n\n");
+
         ledger_output
     }
 }
