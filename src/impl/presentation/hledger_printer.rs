50f908e726e9beecdf72b7e8a64c63252c79abc1
Mart van Buren <mart@fractic.io>
2025-03-21 21:48:24 +0000

    [presentation] Correct declarations for 0-decimal commodities.

--- src/impl/presentation/hledger_printer.rs
+++ src/impl/presentation/hledger_printer.rs
@@ -100,6 +100,7 @@ impl HledgerPrinter {
     }
 
     fn print_commodities(&self, ledger_output: &mut String, financial_records: &FinancialRecords) {
+        const SAMPLE_AMOUNT: f64 = 1000.0;
         let currencies: HashSet<Currency> = financial_records
             .transactions
             .iter()
@@ -108,11 +109,17 @@ impl HledgerPrinter {
         let sorted_commodity_declarations = {
             let mut v: Vec<String> = currencies
                 .iter()
-                .map(|c| format!("commodity {}\n", self.format_amount(1000.0, *c)))
+                .map(|c| {
+                    format!(
+                        "commodity {}\n",
+                        self.format_amount(SAMPLE_AMOUNT, *c, true)
+                    )
+                })
                 .collect();
             v.sort();
             v
         };
+        ledger_output.push_str("decimal-mark .\n");
         for d in sorted_commodity_declarations {
             ledger_output.push_str(&d);
         }
@@ -156,7 +163,7 @@ impl HledgerPrinter {
                 ledger_output.push_str(&format!(
                     "    {:55} {:>20}\n",
                     posting.account.ledger(),
-                    self.format_amount(posting.amount, posting.currency),
+                    self.format_amount(posting.amount, posting.currency, false),
                 ));
             }
             for annotation in financial_records
@@ -187,17 +194,38 @@ impl HledgerPrinter {
             ledger_output.push_str(&format!(
                 "    {:50} 0 == {:>20}\n",
                 assertion.account.ledger(),
-                self.format_amount(assertion.balance, assertion.currency),
+                self.format_amount(assertion.balance, assertion.currency, false),
             ));
             ledger_output.push('\n');
         }
     }
 
-    fn format_amount(&self, amount: f64, currency: Currency) -> String {
-        let decimal_places = currency.exponent().unwrap_or(0) as usize;
+    /// Standard number decimal places for the given currency
+    /// (ex. JPY = 0, USD = 2).
+    fn decimal_places(&self, currency: Currency) -> usize {
+        currency.exponent().unwrap_or(0) as usize
+    }
+
+    /// Format cash amount with currency symbol, correct number of decimal places,
+    /// and proper thousands separators.
+    ///
+    /// For consistency, uses en locale ('.' as decimal mark, i.e. 1,000.00)
+    /// regardless of user's locale or currency. Could be generalized in the
+    /// future.
+    ///
+    /// For currencies with 0 decimal places, a decimal mark is only included if
+    /// 'trailing_decimal' is true. For other currencies, this flag has no
+    /// effect.
+    fn format_amount(&self, amount: f64, currency: Currency, trailing_decimal: bool) -> String {
+        let decimal_places = self.decimal_places(currency);
         if decimal_places == 0 {
             let amount_rounded = (amount.round() as i64).to_formatted_string(&Locale::en);
-            return format!("{} {}", amount_rounded, currency.symbol());
+            return format!(
+                "{}{} {}",
+                amount_rounded,
+                if trailing_decimal { "." } else { "" },
+                currency.symbol()
+            );
         } else {
             let amount_integer_part = (amount.trunc() as i64).to_formatted_string(&Locale::en);
             let amount_fractional_part = format!("{:.decimal_places$}", amount.fract())
