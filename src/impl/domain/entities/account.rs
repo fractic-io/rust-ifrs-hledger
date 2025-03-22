48f8322c1297c02c3e0a6c92d8c267d614008d0b
Mart van Buren <mart@fractic.io>
2025-03-22 08:51:53 +0000

    WIP: First iteration of account classification.

--- src/impl/domain/entities/account.rs
+++ src/impl/domain/entities/account.rs
@@ -1,12 +1,100 @@
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub enum Account {
-    Asset(AssetAccount),
-    Cash(CashAccount),
-    Liability(LiabilityAccount),
-    Income(IncomeAccount),
-    Expense(ExpenseAccount),
-    Equity(EquityAccount),
-    CapitalStock(CapitalStockAccount),
+    Asset(AssetAccount, AssetClassification),
+    Liability(LiabilityAccount, LiabilityClassification),
+    Income(IncomeAccount, IncomeClassification),
+    Expense(ExpenseAccount, ExpenseClassification),
+    Equity(EquityAccount, EquityClassification),
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub enum AssetClassification {
+    // Current.
+    CashAndCashEquivalents,
+    AccountsReceivable,
+    Inventory,
+    PrepaidExpenses,
+    ShortTermInvestments,
+    ShortTermDeposits,
+    OtherCurrent,
+
+    // Non-current.
+    PropertyPlantEquipment,
+    IntangibleAssets,
+    LongTermInvestments,
+    LongTermDeposits,
+    DeferredIncomeTax,
+    OtherNonCurrent,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub enum LiabilityClassification {
+    // Current.
+    AccountsPayable,
+    AccruedExpenses,
+    DeferredRevenue,
+    ShortTermDebt,
+    OtherCurrent,
+
+    // Non-current.
+    LongTermDebt,
+    DeferredIncomeTax,
+    OtherNonCurrent,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub enum IncomeClassification {
+    // Operating (core business) revenues.
+    SalesRevenue,   // Revenue from selling goods.
+    ServiceRevenue, // Revenue from providing services.
+
+    // Financing and non-operating revenues.
+    InterestIncome, // Interest earned on deposits or investments.
+    DividendIncome, // Dividends received from investments.
+    RentalIncome,   // Income from rental properties.
+
+    // Gains from non-core activities.
+    GainOnSaleOfAssets, // Gains from the sale of long-term assets.
+    FxGain,             // Gains from foreign exchange transactions.
+
+    OtherIncome,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub enum ExpenseClassification {
+    // Direct costs for production.
+    CostOfGoodsSold, // Direct costs associated with production or purchase of goods.
+
+    // Operating expenses.
+    SellingExpenses, // Expenses related to selling, such as marketing, advertising, and sales commissions.
+    GeneralAdministrativeExpenses, // Overhead costs, including office expenses, rent, utilities, and salaries of non-sales staff.
+    ResearchAndDevelopmentExpenses, // Costs incurred for R&D activities.
+    CloudServiceExpense,           // Costs incurred for cloud services.
+
+    // Depreciation & Amortization.
+    DepreciationExpense, // Allocation of cost for tangible assets.
+    AmortizationExpense, // Allocation of cost for intangible assets.
+
+    // Financing and non-operating expenses.
+    InterestExpense,  // Costs incurred on borrowings.
+    IncomeTaxExpense, // Income tax expense.
+
+    // Losses from non-core activities.
+    LossOnSaleOfAssets, // Losses from the sale of long-term assets.
+    FxLoss,             // Losses from foreign exchange transactions.
+
+    OtherExpenses,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub enum EquityClassification {
+    // Contributed capital.
+    CommonStock,
+    PreferredStock,
+    TreasuryStock,
+
+    // Earned capital.
+    RetainedEarnings,
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
