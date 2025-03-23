0e285a4b1289e37ea8a6b45b2f3b385bffcf74f8
Mart van Buren <mart@fractic.io>
2025-03-23 17:05:09 +0000

    [presentation] Display negative values in parentheses.

--- src/impl/presentation/utils.rs
+++ src/impl/presentation/utils.rs
@@ -3,24 +3,30 @@ use num_format::{Locale, ToFormattedString as _};
 
 /// Standard number decimal places for the given currency
 /// (ex. JPY = 0, USD = 2).
-pub(crate) fn decimal_places(currency: Currency) -> usize {
+fn decimal_places(currency: Currency) -> usize {
     currency.exponent().unwrap_or(0) as usize
 }
 
 /// Format cash amount with currency symbol, correct number of decimal places,
-/// and proper thousands separators.
+/// proper thousands separators, surrounding parentheses for negative amounts,
+/// and optional trailing decimal mark.
 ///
 /// For consistency, uses en locale ('.' as decimal mark, i.e. 1,000.00)
-/// regardless of user's locale or currency. Could be generalized in the
-/// future.
+/// regardless of user's locale or currency. Could be generalized in the future.
 ///
-/// For currencies with 0 decimal places, a decimal mark is only included if
-/// 'trailing_decimal' is true. For other currencies, this flag has no
-/// effect.
+/// For currencies with 0 decimal places, a decimal mark is always included if
+/// 'trailing_decimal' is true. For other currencies, this flag has no effect.
 pub(crate) fn format_amount(amount: f64, currency: Currency, trailing_decimal: bool) -> String {
     let decimal_places = decimal_places(currency);
     if decimal_places == 0 {
-        let amount_rounded = (amount.round() as i64).to_formatted_string(&Locale::en);
+        let amount_rounded = {
+            let abs = (amount.abs().round() as i64).to_formatted_string(&Locale::en);
+            if amount < 0.0 {
+                format!("({})", abs)
+            } else {
+                abs
+            }
+        };
         return format!(
             "{}{} {}",
             amount_rounded,
@@ -28,17 +34,26 @@ pub(crate) fn format_amount(amount: f64, currency: Currency, trailing_decimal: b
             currency.symbol()
         );
     } else {
-        let amount_integer_part = (amount.trunc() as i64).to_formatted_string(&Locale::en);
+        let amount_integer_part = (amount.abs().trunc() as i64).to_formatted_string(&Locale::en);
         let amount_fractional_part = format!("{:.decimal_places$}", amount.fract())
             .split('.')
             .nth(1)
             .map(|f| f.to_string())
             .unwrap_or_default();
-        format!(
-            "{}.{:0decimal_places$} {}",
-            amount_integer_part,
-            amount_fractional_part,
-            currency.symbol(),
-        )
+        if amount < 0.0 {
+            format!(
+                "({}.{:0decimal_places$}) {}",
+                amount_integer_part,
+                amount_fractional_part,
+                currency.symbol(),
+            )
+        } else {
+            format!(
+                "{}.{:0decimal_places$} {}",
+                amount_integer_part,
+                amount_fractional_part,
+                currency.symbol(),
+            )
+        }
     }
 }
